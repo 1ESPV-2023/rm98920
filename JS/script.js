4a160517dced265658e85b62dcab7ef39c53a111
Luan-Nijou <RM98920@fiap.com.br>
2023-04-20 21:17:48 -0300

    Exemplos de banner

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,57 @@
-function mudaCor () {
+// function mudaCor () {
     
-let r = 0
-let g = 0
-let b = 0
+// let r = 0
+// let g = 0
+// let b = 0
 
-//Math 
-r = Math.ceil(Math.random() * 255)
-g = Math.ceil(Math.random() * 255)
-b = Math.ceil(Math.random() * 255)
+// //Math 
+// r = Math.ceil(Math.random() * 255)
+// g = Math.ceil(Math.random() * 255)
+// b = Math.ceil(Math.random() * 255)
 
-const elementos = [...document.getElementsByClassName()];
+// const elementos = [...document.getElementsByClassName()];
 
-    elementos.forEach((cab)=>{
-        cab.style.backgroundColor = `rgb(${r},${g},${b})`
+//     elementos.forEach((cab)=>{
+//         cab.style.backgroundColor = `rgb(${r},${g},${b})`
 
-    })
-    tmp = setTimeout(mudaCor,100);
+//     })
+//     tmp = setTimeout(mudaCor,100);
+// }
+
+// mudaCor()
+mudaBanner1()
+
+function mudaBanner1() {
+    
+    let banner1 = "./img/banner-1440x300-1.jpg";
+    
+
+    const elImg = document.querySelector(".cabecalho > img");
+    elImg.src = banner1;
+    
+    setTimeout(mudaBanner2, 2000);
 }
 
-tmp = setTimeout(mudaCor,100);
+function mudaBanner2() {
+    
+    
+    let banner2 = "./img/banner-1440x300-2.jpg";
+    
+
+    const elImg = document.querySelector(".cabecalho > img");
+    elImg.src = banner1;
+    
+    setTimeout(mudaBanner3, 2000);
+}
+
+function mudaBanner3() {
+    
+    
+    let banner2 = "./img/banner-1440x300-2.jpg";
+    
+
+    const elImg = document.querySelector(".cabecalho > img");
+    elImg.src = banner1;
+    
+    setTimeout(mudaBanner1, 2000);
+}
