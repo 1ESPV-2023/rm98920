d6165a9ed9b4005cc4cae5639bdf430bd6af8d08
Luan-Nijou <RM98920@fiap.com.br>
2023-04-27 20:55:48 -0300

    Prox ex

--- JS/script.js
+++ JS/script.js
@@ -19,39 +19,64 @@
 // }
 
 // mudaCor()
-mudaBanner1()
+// mudaBanner1();
+
+
+// function mudaBanner1() {
+    
+//     let banner1 = "./img/banner-1440x300-1.jpg";
+    
+
+//     const elImg = document.querySelector(".cabecalho > img");
+//     elImg.src = banner1;
+    
+//     setTimeout(mudaBanner2, 2000);
+// }
+
+// function mudaBanner2() {
+       
+//     let banner2 = "./img/banner-1440x300-2.jpg";
+//     const elImg = document.querySelector(".cabecalho > img");
+//     elImg.src = banner2;    
+//     setTimeout(mudaBanner3, 2000);
+// }
+
+// function mudaBanner3() {
+    
+    
+//     let banner2 = "./img/banner-1440x300-3.jpg";    
+//     const elImg = document.querySelector(".cabecalho > img");
+//     elImg.src = banner3;    
+//     setTimeout(mudaBanner1, 2000);
+// }
 
 function mudaBanner1() {
-    
-    let banner1 = "./img/banner-1440x300-1.jpg";
-    
 
-    const elImg = document.querySelector(".cabecalho > img");
-    elImg.src = banner1;
+    let num1 = Math.ceil(Math.random()*3);
+    let num2 = Math.ceil(Math.random()*3);
+
+    let CaminhoImg1 = `./img/banner-lateral-${num1}.png`;
+    let CaminhoImg2 = `./img/banner-lateral-${num2}.png`;
+
+    const b1 = document.querySelector(".banner-1 img");
+    const b2 = document.querySelector(".banner-2 img");
+
+    b1.src = CaminhoImg1;
+    b2.src = CaminhoImg2;
+    setTimeout(mudaBanner1 , 1000)
     
-    setTimeout(mudaBanner2, 2000);
 }
+function acender() {
 
-function mudaBanner2() {
     
+    const gif1 = document.querySelector(".conteudo img");
+    const botao = document.querySelector(".botao");
     
-    let banner2 = "./img/banner-1440x300-2.jpg";
-    
-
-    const elImg = document.querySelector(".cabecalho > img");
-    elImg.src = banner2;
-    
-    setTimeout(mudaBanner3, 2000);
-}
-
-function mudaBanner3() {
-    
-    
-    let banner2 = "./img/banner-1440x300-3.jpg";
-    
-
-    const elImg = document.querySelector(".cabecalho > img");
-    elImg.src = banner3;
-    
-    setTimeout(mudaBanner1, 2000);
+    if(botao.textContent == "Ligar"){
+        gif1.src = "./img/pic_bulbon.gif";
+        botao.textContent = "Desligar";
+    }else{
+        gif1.src = "./img/pic_bulboff.gif";
+        botao.textContent = "Ligar";
+    }
 }
