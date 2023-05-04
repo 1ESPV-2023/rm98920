836a4200a98d3d4ddb96378f45d9881ae82b0c46
Luan-Nijou <RM98920@fiap.com.br>
2023-05-04 20:53:02 -0300

    Validaçao

---
+++ JS/login.js
@@ -0,0 +1,34 @@
+//REcuperando o input após ele receber o focus
+const inputUser = document.querySelector('input[type="text"]')
+const LUser = document.querySelector('label[for="idNm"]')
+
+// inputUser.addEventListener('focus',()=>{
+//     inputUser.setAttribute("style",'outline-color:#ff0000;'); 
+// })
+
+inputUser.addEventListener('keyup',()=>{
+    
+    if(inputUser.value.length < 5){
+        inputUser.setAttribute('style','outline-color:#ff0000;'); 
+        LUser.setAttribute('style','color:#ff0000;'); 
+
+    }else{
+        inputUser.setAttribute('style','outline-color:#00ff00;');
+        LUser.setAttribute('style','color:#00ff00;'); 
+    }
+   
+    
+       
+})
+
+const eyepass = document.querySelector('.fa-eye')
+
+eyepass.addEventListener('click',()=>{
+    const inputPass = document.querySelector('input[name="txtPass"]');
+    if(inputPass.getAttribute("type") == "password"){
+        inputPass.setAttribute("type","text");
+
+    }else{
+        inputPass.setAttribute("type","password");
+    }
+})
